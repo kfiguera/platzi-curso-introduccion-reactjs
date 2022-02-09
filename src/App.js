8299758c81dfa67dab727e840ea8ad2a5bde28f4
Kevin Figuera <kevinalejandro3@gmail.com>
2022-02-09 10:49:33 -0400

    Contando y buscando TODOs

--- src/App.js
+++ src/App.js
@@ -3,19 +3,39 @@ import {TodoCounter, TodoSearch, TodoList, TodoItem, CreateTodoButton} from "./c
 // import logo from './logo.svg';
 //import './App.css';
 
-const todos = [
+const defaultTodos = [
     {id: 1, text: 'Cortar cebolla', completed: false},
     {id: 2, text: 'Tomar el curso', completed: true},
     {id: 3, text: 'Ir a dormir', completed: false},
 ];
 
 function App() {
+    const [todos, setTodos] = React.useState(defaultTodos);
+    const [searchValue, setSearchValue] = React.useState('');
+    const completedTodos = todos.filter(todo => !!todo.completed).length;
+    const totalTodos = todos.length;
+    let searchedTodos = [];
+    if(searchValue.length >= 1){
+        searchedTodos = todos.filter(todo =>{
+            let todoText = todo.text.toLowerCase();
+            let searchText = searchValue.toLowerCase()
+            return todoText.includes(searchText);
+        });
+    }else{
+        searchedTodos = todos;
+    }
     return (
         <React.Fragment>
-            <TodoCounter/>
-            <TodoSearch/>
+            <TodoCounter
+                completed={completedTodos}
+                total={totalTodos}
+            />
+            <TodoSearch
+                searchValue={searchValue}
+                setSearchValue={setSearchValue}
+            />
             <TodoList>
-                {todos.map(todo => (
+                {searchedTodos.map(todo => (
                     <TodoItem
                         key={todo.id}
                         text={todo.text}
