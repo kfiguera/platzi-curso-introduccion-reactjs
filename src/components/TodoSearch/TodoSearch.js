0986d4c3e6df507938fd0b6e6edb18a18bbc2f4c
Kevin Figuera <kevinalejandro3@gmail.com>
2022-02-09 10:12:31 -0400

    Manejo del estado

--- src/components/TodoSearch/TodoSearch.js
+++ src/components/TodoSearch/TodoSearch.js
@@ -2,16 +2,21 @@ import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch(props) {
+    const [searchValue, setSearchValue] = React.useState('');
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
+        setSearchValue(event.target.value)
     }
-    return (
-
+    return [
         <input
             className="TodoSearch"
+            placeholder="Cebolla"
+            value={searchValue}
             onChange={onSearchValueChange}
-            type="text" placeholder="Cebolla"/>
-    )
+        />,
+        <p>{searchValue}</p>
+    ];
+
 }
 
 export {TodoSearch};
