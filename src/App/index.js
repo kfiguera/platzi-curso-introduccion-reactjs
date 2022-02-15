55087ae85b943590840d1bf0e5ca6684164c97d3
Kevin Figuera <kevinalejandro3@gmail.com>
2022-02-15 17:41:28 -0400

    Persistencia de datos con Local Storage

--- src/App/index.js
+++ src/App/index.js
@@ -4,14 +4,23 @@ import {AppUi} from "./AppUi";
 // import logo from './logo.svg';
 //import './App.css';
 
-const defaultTodos = [
-    {id: uuidv4(), text: 'Cortar cebolla', completed: false},
-    {id: uuidv4(), text: 'Tomar el curso', completed: true},
-    {id: uuidv4(), text: 'Ir a dormir', completed: false},
-];
+ const defaultTodos = [
+     {id: uuidv4(), text: 'Cortar cebolla', completed: false},
+     {id: uuidv4(), text: 'Tomar el curso', completed: true},
+     {id: uuidv4(), text: 'Ir a dormir', completed: false},
+ ];
 
 function App() {
-    const [todos, setTodos] = React.useState(defaultTodos);
+    const localStorageTodos = localStorage.getItem('TODOS_V1');
+    let parsedTodos;
+    if (!localStorageTodos) {
+        localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
+        parsedTodos = defaultTodos;
+    } else {
+        parsedTodos = JSON.parse(localStorageTodos);
+    }
+
+    const [todos, setTodos] = React.useState(parsedTodos);
     const [searchValue, setSearchValue] = React.useState('');
     const completedTodos = todos.filter(todo => !!todo.completed).length;
     const totalTodos = todos.length;
@@ -25,7 +34,11 @@ function App() {
     } else {
         searchedTodos = todos;
     }
-
+    const saveTodos = (newTodos) => {
+        const stringigiedTodos = JSON.stringify(newTodos);
+        localStorage.setItem('TODOS_V1', stringigiedTodos);
+        setTodos(newTodos);
+    }
     const toggleCompleteTodos = (id) => {
         const todoIndex = todos.findIndex(todo => todo.id === id);
         const newTodos = [...todos];
@@ -34,7 +47,7 @@ function App() {
             ? newTodos[todoIndex].completed = true
             : newTodos[todoIndex].completed = false;
 
-        setTodos(newTodos);
+        saveTodos(newTodos);
     };
     const deleteTodo = (id) => {
         const todoIndex = todos.findIndex(todo => todo.id === id);
@@ -42,7 +55,7 @@ function App() {
         console.log(newTodos);
         newTodos.splice(todoIndex, 1);
         console.log(newTodos);
-        setTodos(newTodos);
+        saveTodos(newTodos);
     };
 
     return (
